ad71e64ff252bab15b0240b61319b0373a3bd825
Alexander Granovski <alexxgranovski@gmail.com>
2020-02-07 03:56:14 +0300

    small fix

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,8 +8,8 @@ program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
-  .parse(process.argv)
   .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv)
   .action((firstConfig, secondConfig) => {
     console.log(genDiff(firstConfig, secondConfig, program.format));
   })
--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -4,18 +4,18 @@ const render = (ast) => {
       return `${acc}${' '.repeat(space2)}${i.name}: {\n${iter(i.children, space1 + 4, space2 + 4)}${' '.repeat(space2)}}\n`;
     }
     if (i.status === 'updated') {
-      let confStr1 = `${' '.repeat(space1)}- ${i.name}: ${i.value[0]}\n`;
-      let confStr2 = `${' '.repeat(space1)}+ ${i.name}: ${i.value[1]}\n`;
+      let remove = `${' '.repeat(space1)}- ${i.name}: ${i.value[0]}\n`;
+      let add = `${' '.repeat(space1)}+ ${i.name}: ${i.value[1]}\n`;
       if (i.value[0] instanceof Object) {
         const valueKeys = Object.keys(i.value[0]);
-        const value = valueKeys.reduce((ac, k) => `${ac}${' '.repeat(space2 + 4)}${k}: ${i.value[0][k]}\n`, '');
-        confStr1 = `${' '.repeat(space1)}- ${i.name}: {\n${value}${' '.repeat(space2)}}\n`;
+        const value = valueKeys.reduce((ac, key) => `${ac}${' '.repeat(space2 + 4)}${key}: ${i.value[0][key]}\n`, '');
+        remove = `${' '.repeat(space1)}- ${i.name}: {\n${value}${' '.repeat(space2)}}\n`;
       } else if (i.value[1] instanceof Object) {
         const valueKeys = Object.keys(i.value[1]);
         const value = valueKeys.reduce((ac, k) => `${ac}${' '.repeat(space2 + 4)}${k}: ${i.value[1][k]}\n`, '');
-        confStr2 = `${' '.repeat(space1)}+ ${i.name}: {\n${value}${' '.repeat(space2)}}\n`;
+        add = `${' '.repeat(space1)}+ ${i.name}: {\n${value}${' '.repeat(space2)}}\n`;
       }
-      return `${acc}${confStr1}${confStr2}`;
+      return `${acc}${remove}${add}`;
     }
     if (i.status === 'add') {
       if (i.value instanceof Object) {
