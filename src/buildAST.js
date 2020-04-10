0e79a89603f3cb716de9ee676cc5e40d82e39869
Alexander Granovski <alexxgranovski@gmail.com>
2020-04-10 05:31:45 +0300

    refactoring

--- src/buildAST.js
+++ src/buildAST.js
@@ -11,7 +11,7 @@ const buildAST = (firstConfig, secondConfig) => {
     const keys = _.union(_.keys(obj1), _.keys(obj2));
     return keys.map((key) => {
       if (_.has(obj1, key) && _.has(obj2, key)) {
-        if (obj1[key] instanceof Object && obj2[key] instanceof Object) {
+        if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
           return buildNode(key, 'parent', '', iter(obj1[key], obj2[key]));
         }
         if (obj1[key] === obj2[key]) {
